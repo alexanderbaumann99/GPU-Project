87368d15f619d0e4a6235f44897916057fe721a0
alexanderbaumann99 <alex.baumi@freenet.de>
2022-03-11 23:52:57 +0100

    Copied some functions from cuda file

--- regression.py
+++ regression.py
@@ -2,6 +2,78 @@ import numpy as np
 from os.path import join
 import torch
 
+#copy from MC.cu
+def params():
+    
+        Tgc=np.empty((15,))
+        rgc=np.empty((15,))
+            
+        d = 1/360
+        w = 7.0 * d
+        m = 30. * d
+        y = 12. * m
+        
+        Tgc[0] = d
+        Tgc[1] = 2*d
+        Tgc[2] = w
+        Tgc[3] = 2.*w
+        Tgc[4] = m
+        Tgc[5] = 2*m
+        Tgc[6] = 3*m
+        Tgc[7] = 6*m
+        Tgc[8] = y
+        Tgc[9] = y + 3*m
+        Tgc[10] = y + 6*m
+        Tgc[11] = 2*y
+        Tgc[12] = 2*y + 6*m
+        Tgc[13] = 3*y
+        Tgc[14] = 3*y + 6.*m
+
+        rgc[0] = 0.05
+        rgc[1] = 0.07
+        rgc[2] = 0.08
+        rgc[3] = 0.06
+        rgc[4] = 0.07
+        rgc[5] = 0.1
+        rgc[6] = 0.11
+        rgc[7] = 0.13
+        rgc[8] = 0.12
+        rgc[9] = 0.14
+        rgc[10] = 0.145
+        rgc[11] = 0.14
+        rgc[12] = 0.135
+        rgc[13] = 0.13
+        rgc[14] = 0.*y
+
+        return Tgc,rgc
+
+
+Tg,rg=params()
+
+#copy from MC.cu
+def rt_int(t,T,i,j):
+
+        if(i==j):
+            res = (T-t)*rg[i]
+        else:
+            res = (T-Tg[j-1])*rg[j] + (Tg[i]-t)*rg[i]
+                        
+            for k in range(i+1,j):
+                res += (Tg[k]-Tg[k-1])*rg[k]
+
+        return res
+
+#copy from MC.cu
+def timeIdx(t):
+     
+        for i in range(14,-1,-1):
+            if(t<Tg[i]):
+                I = i           
+            
+        return I
+
+
+
 class LinearRegression:
     def __init__(self, p):
         self.weights = np.zeros(p+1) # +1 for bias
@@ -23,10 +95,20 @@ class LinearRegression:
         self.weights = np.linalg.inv(r)@q.T.dot(y)
 
     def predict(self, x):
-        return self.augment(x).dot(self.weighs)
+        return self.augment(x).dot(self.weights)
 
     def loss(self, x, y):
         return np.sum(np.square(self.predict(x)-y))
+
+    def compute_value(self,x,t):
+
+        prediction=self.predict(x)
+        q=timeIdx(1.0)
+        disc_pred=np.exp(-rt_int(t,1.0,0,q))*prediction
+
+        return disc_pred
+
+
 
 
 file_dir = join('.', 'nestedMC_data1') # does it work on windows?
